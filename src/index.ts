f89a07d7713f04fc06b6da1e01949252f7df6a05
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-08-27 16:01:41 +0600

    feat: initial commit
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import core from "@actions/core";
+import github from "@actions/github";
+import { execSync } from "child_process";
+
+async function run() {
+    console.log(JSON.stringify(github, null, 2));
+    console.log(core.getInput("after"));
+    console.log(core.getInput("before"));
+    const commits = JSON.parse(
+        "[" +
+            execSync(
+                `git log --pretty=format:'{"sha":"%H","message":"%s"},' ${github.context.event.before}`,
+            )
+                .toString()
+                .slice(0, -1) +
+            "]",
+    );
+
+    if (commits.length > 0) {
+        const firstCommit = commits[0];
+        const lastCommit = commits[commits.length - 1];
+
+        for (const commit of commits) {
+            console.log(`Commit: ${commit.sha} - ${commit.message}`);
+        }
+    } else {
+        console.log("No commits found.");
+    }
+}
+
+run()
+    .then()
+    .catch((error) => core.setFailed(error));
