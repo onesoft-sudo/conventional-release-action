233d81359babad76a52de276f60ccbfa8e68c0a7
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-08-27 19:51:33 +0600

    feat: finalize version bumping

---
+++ src/VersionManager.ts
@@ -0,0 +1,125 @@
+import { readFile, writeFile } from "fs/promises";
+import * as semver from "semver";
+import * as core from "@actions/core";
+
+export type Commit = {
+    message: string;
+    sha: string;
+};
+
+class VersionManager {
+    private allowedCommitTypes = [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+    ];
+
+    private commits: Commit[] = [];
+
+    public setAllowedCommitTypes(allowedCommitTypes: string[]): this {
+        this.allowedCommitTypes = allowedCommitTypes;
+        return this;
+    }
+
+    public setCommits(commits: Commit[]): this {
+        this.commits = commits;
+        return this;
+    }
+
+    public async bump(lastVersion: string) {
+        const parsed = semver.parse(lastVersion, { loose: true });
+        let build: string[] = [];
+        let suffix: string[] = [];
+
+        if (!parsed) {
+            throw new Error(`Failed to parse version "${lastVersion}".`);
+        }
+
+        parsed.build = [];
+
+        for (const commit of this.commits.toReversed()) {
+            const newlineIndex = commit.message.indexOf("\n");
+            const head = commit.message.slice(
+                0,
+                newlineIndex === -1 ? undefined : newlineIndex,
+            );
+            let [type] = head.split(":");
+            let increased = false;
+            let major = false;
+            const forcePrerelease =
+                /\[(v\:)?(alpha|beta|rc|prerelease)\]/gi.test(commit.message);
+
+            if (type.endsWith("!")) {
+                type = type.slice(0, -1);
+                major = true;
+            }
+
+            type = type.trim();
+            type = type.includes("(") ? type.slice(0, type.indexOf("(")) : type;
+            type = type.toLowerCase();
+
+            if (!this.allowedCommitTypes.includes(type)) {
+                continue;
+            }
+
+            const buildMetadata = commit.message.match(
+                /\nBuild-metadata: (.*)/i,
+            );
+            const versionSuffix = commit.message.match(
+                /\nVersion-suffix: (.*)/i,
+            );
+
+            if (major) {
+                parsed.inc(forcePrerelease ? "premajor" : "major");
+                increased = true;
+            } else {
+                if (type === "feat") {
+                    parsed.inc(forcePrerelease ? "preminor" : "minor");
+                    increased = true;
+                }
+
+                if (type === "fix") {
+                    parsed.inc(forcePrerelease ? "prepatch" : "patch");
+                    increased = true;
+                }
+            }
+
+            if (forcePrerelease && !increased) {
+                parsed.inc("prerelease");
+                increased = true;
+            }
+
+            if (versionSuffix) {
+                if (!increased) {
+                    parsed.inc("prerelease");
+                    increased = true;
+                }
+
+                suffix = versionSuffix[1].split(".");
+            }
+
+            if (buildMetadata) {
+                if (!increased) {
+                    parsed.inc("prerelease");
+                    increased = true;
+                }
+
+                build = buildMetadata[1].split(".");
+            }
+        }
+
+        const newVersion =
+            parsed.toString() +
+            (suffix?.length ? `${suffix.join(".")}` : "") +
+            (build?.length ? `+${build.join(".")}` : "");
+
+        return newVersion;
+    }
+}
+
+export default VersionManager;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,122 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { execSync } from "child_process";
+import VersionManager, { Commit } from "./VersionManager";
+import { readFile, writeFile } from "fs/promises";
+import * as crypto from "crypto";
+
+type VersionManagerModule = {
+    resolver?: (versionManager: VersionManager) => string | Promise<string>;
+    updater?: (
+        versionManager: VersionManager,
+        version: string,
+    ) => void | Promise<void>;
+};
 
 async function run() {
-    console.log(JSON.stringify(github, null, 2));
-    console.log(core.getInput("after"));
-    console.log(core.getInput("before"));
-    const commits = JSON.parse(
-        "[" +
-            execSync(
-                `git log --pretty=format:'{"sha":"%H","message":"%s"},' ${""}`,
-            )
-                .toString()
-                .slice(0, -1) +
-            "]",
-    );
+    const allowedCommitTypes = core
+        .getInput("allowed-commit-types")
+        .split(",")
+        .filter(Boolean);
+    const versionJsonFile =
+        core.getInput("version-json-file") || "package.json";
+    const versionManagerModulePath = core.getInput("version-manager-module");
+    const jsonTabWidth = parseInt(core.getInput("json-tab-width") || "4");
 
-    if (commits.length > 0) {
-        const firstCommit = commits[0];
-        const lastCommit = commits[commits.length - 1];
+    const { after, before } = github.context.payload;
+    const boundary = `${crypto.randomBytes(16).toString("hex")}`;
+    const output = execSync(
+        `git log --pretty=format:'%H %B${boundary}' ${before}..${after}`,
+    ).toString();
 
-        for (const commit of commits) {
-            console.log(`Commit: ${commit.sha} - ${commit.message}`);
+    core.debug(`Output: ${output}`);
+
+    const commits: Commit[] = [];
+
+    for (const commit of output.split(boundary)) {
+        if (!commit.trim()) {
+            continue;
         }
-    } else {
-        console.log("No commits found.");
+
+        const space = commit.indexOf(" ");
+
+        if (space === -1) {
+            core.error("Failed to parse commit: " + commit);
+            continue;
+        }
+
+        const sha = commit.slice(0, space);
+        const message = commit.slice(space + 1).trim();
+
+        if (!sha || !message) {
+            core.error("Failed to parse commit: " + commit);
+            continue;
+        }
+
+        commits.push({ sha, message });
     }
+
+    if (commits.length === 0) {
+        core.info("No new commits found.");
+        return;
+    }
+
+    core.info("New commits found:");
+
+    for (const commit of commits) {
+        core.info(`- ${commit.sha}: ${commit.message}`);
+    }
+
+    const versionManager = new VersionManager();
+
+    if (allowedCommitTypes.length > 0) {
+        versionManager.setAllowedCommitTypes(allowedCommitTypes);
+    }
+
+    versionManager.setCommits(commits);
+
+    const versionManagerModule: VersionManagerModule = versionManagerModulePath
+        ? await import(versionManagerModulePath)
+        : null;
+
+    const getLastVersion =
+        versionManagerModule?.resolver ??
+        (async () => {
+            const packageJson = JSON.parse(
+                await readFile(versionJsonFile, "utf-8"),
+            );
+
+            if (!packageJson.version) {
+                throw new Error(
+                    `Version file "${versionJsonFile}" does not contain a version field.`,
+                );
+            }
+
+            return packageJson.version as string;
+        });
+
+    const updateVersion =
+        versionManagerModule?.updater ??
+        (async (_versionManager, version) => {
+            const packageJson = JSON.parse(
+                await readFile(versionJsonFile, "utf-8"),
+            );
+
+            packageJson.version = version;
+
+            await writeFile(
+                versionJsonFile,
+                JSON.stringify(packageJson, null, jsonTabWidth) + "\n",
+            );
+        });
+
+    const currentVersion = await getLastVersion(versionManager);
+    core.info(`Current version: ${currentVersion}`);
+
+    const updatedVersion = await versionManager.bump(currentVersion);
+
+    core.info(`Updated version: ${updatedVersion}`);
+    await updateVersion(versionManager, updatedVersion);
 }
 
 run()
