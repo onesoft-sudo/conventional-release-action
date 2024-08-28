3898a29a696cf7cf310b77b6a19b4e5910e239e7
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-08-28 22:38:52 +0600

    fix: rebuild
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/GitClient.ts
+++ src/GitClient.ts
@@ -42,13 +42,13 @@ class GitClient implements AsyncDisposable {
 
         return new Promise<string>((resolve, reject) => {
             process.on("exit", (code) => {
-                if (code !== 0) {
+                if (code === null || code !== 0) {
                     console.log("Failed to execute git command: ");
                     console.error(error);
+
                     reject(
                         new Error(`Failed to execute git command: ${error}`),
                     );
-
                     return;
                 }
 
