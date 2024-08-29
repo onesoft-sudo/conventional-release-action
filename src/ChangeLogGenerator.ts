74b68a5877cd45b15d099743a8148dbb59f2ce2d
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-08-29 13:31:33 +0600

    fix: commit messages format
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/ChangeLogGenerator.ts
+++ src/ChangeLogGenerator.ts
@@ -80,7 +80,7 @@ class ChangeLogGenerator implements AsyncDisposable {
                     headerAdded = true;
                 }
 
-                notes += `* [[${commit.shortId}](https://github.com/${githubUsername}/${githubRepo}/commit/${commit.id})] **${typeWithSubject[1]}**: ${head.replace(/^([A-Za-z0-9-_](.*?))\!?:/, "").trim()}\n`;
+                notes += `* \`[[${commit.shortId}](https://github.com/${githubUsername}/${githubRepo}/commit/${commit.id})]\` **${typeWithSubject[1]}**: ${head.replace(/^([A-Za-z0-9-_](.*?))\!?:/, "").trim()}\n`;
             }
 
             if (headerAdded) {
